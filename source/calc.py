e02c72d20ccf54cceca9931de04ddaf0bb56b267
Daniel Park <parkd7@rpi.edu>
2024-08-04 11:32:38 -0500

    Refactor

--- source/calc.py
+++ source/calc.py
@@ -1,10 +1,11 @@
+import math, secrets
+
+
 def calc_width(n: int) -> int:
-    import math
     return math.floor(math.log10(n-1)) + 1
 
 
 def scramble_str(s: str) -> str:
-    import secrets
     s_list = list(s)
     secrets.SystemRandom().shuffle(s_list)
     return ''.join(s_list)
