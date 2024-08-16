d1990b1094ad79c34adb376912a5c0c5f3006e30
Daniel Park <parkd7@rpi.edu>
2024-08-16 18:31:32 -0500

    Add repetition of the program

--- source/main.py
+++ source/main.py
@@ -7,76 +7,69 @@ if __name__ == '__main__':
     print("─" * os.get_terminal_size().columns)
 
     output_str = ''
+    repeat = True
 
-    filename = input("Save File Name (Empty String == No Save): ")
-    token = input("Token: ")
-    mode = input("Mode [Random/Password]: ").lower()
-    if mode not in ['random', 'r', 'password', 'p']:
-        raise RuntimeError("Invalid mode.")
+    while repeat:
+        filename = input("Save File Name (Empty String == No Save): ")
+        token = input("Token: ")
+        mode = input("Mode [Random/Password]: ").lower()
+        if mode not in ['random', 'r', 'password', 'p']:
+            raise RuntimeError("Invalid mode.")
 
-    if mode == 'random':
-        num_random = int(input("Number of Random Numbers (MAX == 100,000): "))
+        if mode == 'random':
+            num_random = int(input("Number of Random Numbers (MAX == 100,000): "))
 
-        if not 0 < num_random <= 100_000:
-            raise RuntimeError("Limit exceeded.")
+            if not 0 < num_random <= 100_000:
+                raise RuntimeError("Limit exceeded.")
 
-        minimum = int(input("Minimum: "))
-        maximum = int(input("Maximum: "))
+            minimum = int(input("Minimum: "))
+            maximum = int(input("Maximum: "))
 
-        if minimum > maximum:
-            raise RuntimeError("Invalid min/max.")
+            if minimum > maximum:
+                raise RuntimeError("Invalid min/max.")
 
-        min_number_qubits = util.calc_min_number_qubits(minimum, maximum)
+            min_number_qubits = util.calc_min_number_qubits(minimum, maximum)
 
-        print("─" * os.get_terminal_size().columns)
-        print("Input complete!")
+            print("─" * os.get_terminal_size().columns)
+            print("Input complete!")
 
-        result = circuit.execute(token, min_number_qubits, 1)
+            result = circuit.execute(token, min_number_qubits, 1)
 
-        print("─" * os.get_terminal_size().columns)
+            print("─" * os.get_terminal_size().columns)
 
-        output_str += output.output_random(result, num_random, minimum, maximum)
+            output_str += output.output_random(result, num_random, minimum, maximum)
 
-    else: # mode == 'password'
-        len_password = int(input("Length of Passwords (MAX == 100,000): "))
+        else: # mode == 'password'
+            len_password = int(input("Length of Passwords (MAX == 100,000): "))
 
-        if not 0 < len_password <= 100_000:
-            raise RuntimeError("Limit exceeded.")
+            if not 0 < len_password <= 100_000:
+                raise RuntimeError("Limit exceeded.")
 
-        num_password = int(input("Number of Passwords: "))
+            num_password = int(input("Number of Passwords: "))
 
-        positive = ['y', 'yes', 'true']
-        negative = ['n', 'no', 'false']
+            digits = util.positive_negative(input("Digits? [Y/N]: ").lower())
+            ascii = util.positive_negative(input("ASCII Letters? [Y/N]: ").lower())
+            punc = util.positive_negative(input("Punctuations? [Y/N]: ").lower())
+            
+            settings = (digits, ascii, punc)
+            if True not in settings:
+                raise RuntimeError("No passwords can be generated with these settings.")
 
-        digits = input("Digits? [Y/N]: ").lower()
-        if digits not in positive and digits not in negative:
-            raise RuntimeError("Invalid option.")
-        ascii = input("ASCII Letters? [Y/N]: ").lower()
-        if ascii not in positive and ascii not in negative:
-            raise RuntimeError("Invalid option.")
-        punc = input("Punctuations? [Y/N]: ").lower()
-        if punc not in positive and punc not in negative:
-            raise RuntimeError("Invalid option.")
-        
-        settings = (True if digits in positive else False, 
-                    True if ascii in positive else False, 
-                    True if punc in positive else False)
-        if True not in settings:
-            raise RuntimeError("No passwords can be generated with these settings.")
+            print("─" * os.get_terminal_size().columns)
+            print("Input complete!")
 
-        print("─" * os.get_terminal_size().columns)
-        print("Input complete!")
+            result = circuit.execute(token, 127, num_password)
+            
+            print("─" * os.get_terminal_size().columns)
 
-        result = circuit.execute(token, 127, num_password)
-        
-        print("─" * os.get_terminal_size().columns)
+            output_str += output.output_password(result, num_password, len_password, settings)
 
-        output_str += output.output_password(result, num_password, len_password, settings)
+        print(output_str, end='')
+        if (filename != ''):
+            with open(filename, 'w') as file:
+                file.write(output_str)
 
-    print(output_str, end='')
-    if (filename != ''):
-        with open(filename, 'w') as file:
-            file.write(output_str)
+        repeat = util.positive_negative(input("Repeat [Y/N]: ").lower())
 
     print("─" * os.get_terminal_size().columns)
 
--- source/util.py
+++ source/util.py
@@ -1,6 +1,18 @@
 import math, secrets
 
 
+def positive_negative(argument: str) -> bool:
+    positive = ['y', 'yes', 'true']
+    negative = ['n', 'no', 'false']
+
+    if argument in positive:
+        return True
+    elif argument in negative:
+        return False
+    else:
+        raise RuntimeError("Invalid option.")
+
+
 def calc_min_number_qubits(minimum: int, maximum: int) -> int:
     return math.ceil(math.log2(abs(maximum-minimum)+1))
 
